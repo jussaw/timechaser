8996624337f17609183a0b6cdb680f724202ba68
Justin Sawyer <justin.sawyer@rtx.com>
2024-06-12 18:12:19 -0500

    Readd Sandbox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,8 @@ export default function App() {
             />
             <Route path="/timesheet" element={<Timesheet />} />
             <Route path="/profile" element={<Profile />} />
+            {/* TODO: Delete Later. Only for Sandboxing */}
+            <Route path="/sandbox" element={<Sandbox />} />
           </Routes>
         </div>
       </div>
