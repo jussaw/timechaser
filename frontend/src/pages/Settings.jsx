5eebb5d097ad1213798c8427a39ad4fbcaa92ee7
Justin Sawyer <justin.sawyer@rtx.com>
2024-06-12 20:36:05 -0500

    Remove Settings

--- frontend/src/pages/Settings.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Settings() {
-  return (
-    <div className="full-page-component flex-grow">
-      <div className="flex h-full items-center justify-center">Settings</div>
-    </div>
-  );
-}
