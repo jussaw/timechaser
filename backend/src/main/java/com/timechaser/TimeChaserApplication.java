3454b23010f0d7435ee8e1037ee9e1a0be456013
Hugh Williams Jr <hugh.r.williams@rtx.com>
2024-06-18 11:25:37 -0600

    Initial backend commit

---
+++ backend/src/main/java/com/timechaser/TimeChaserApplication.java
@@ -0,0 +1,13 @@
+package com.timechaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeChaserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeChaserApplication.class, args);
+	}
+
+}
