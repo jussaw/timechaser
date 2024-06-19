cc51866bca14dafd3a6fb22db28ac169cad71c5f
e40068087 <Jonah.Leung@rtx.com>
2024-06-18 19:24:44 -0500

    Add color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-orange": "#FB923C",
+        "custom-blue": {
+          DEFAULT: "#3B82F6",
+          dark: "#1E3A8A",
+        },
+        "custom-gray": "#9BA1B3",
+        "custom-brown": "#968586",
+        "custom-yellow": "#B4A58C",
+        "custom-white": "#E1E5F2",
+      },
+    },
   },
   plugins: [],
 };
