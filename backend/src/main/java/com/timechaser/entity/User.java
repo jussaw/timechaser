545b5431d6ebce6a7d99f952c4a0d265529d6b7b
Hugh Williams Jr <hugh.r.williams@rtx.com>
2024-06-19 10:36:52 -0600

    Enabled lombok in application

--- backend/src/main/java/com/timechaser/entity/User.java
+++ backend/src/main/java/com/timechaser/entity/User.java
@@ -5,7 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User {
 	@Id
 	@GeneratedValue(strategy  = GenerationType.IDENTITY)
@@ -14,46 +21,5 @@ public class User {
 	private String password;
 	private String firstName;
 	private String lastName;
-	private boolean deleted;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public boolean isDeleted() {
-		return deleted;
-	}
-	public void setDeleted(boolean deleted) {
-		this.deleted = deleted;
-	}
-	
-	
-	
-	
+	private boolean deleted;	
 }
