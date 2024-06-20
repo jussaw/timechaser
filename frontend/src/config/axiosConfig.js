e4a96da1906583c80bf74586925bff983769713e
Justin Sawyer <justin.sawyer@rtx.com>
2024-06-19 21:34:42 -0600

    Add base for axios config

---
+++ frontend/src/config/axiosConfig.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Request interceptor for adding authorization token
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     // Retrieve token from localStorage, cookies, or wherever you store it
+//     const token = localStorage.getItem('accessToken');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// Response interceptor for refreshing token on 401 responses
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     // Handle and refresh token if expired, for example
+//     return response;
+//   },
+//   (error) => {
+//     // Handle 401 Unauthorized responses
+//     if (error.response.status === 401) {
+//       // Logic to handle token refresh or logout user
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+export default axiosInstance;
