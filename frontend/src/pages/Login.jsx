b712c4b31e44fdec6cbbd5f782e5fb8039e2ec0c
Justin Sawyer <justin.sawyer@rtx.com>
2024-06-21 16:30:01 -0600

    Add functionality for input validation and setup for API call

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,80 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClockRotateLeft } from "@fortawesome/free-solid-svg-icons";
 import "../styles/Auth.css";
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [usernameValid, setUsernameValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [focusedField, setFocusedField] = useState(null);
+
+  const usernameInputRef = useRef(null);
+
+  useEffect(() => {
+    usernameInputRef.current = document.getElementById("username");
+    usernameInputRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    const usernameRegex = /^[a-zA-Z0-9._-]{4,}$/;
+    const passwordRegex =
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    const isUsernameValid = usernameRegex.test(formData.username);
+    const isPasswordValid = passwordRegex.test(formData.password);
+
+    setUsernameValid(isUsernameValid);
+    setPasswordValid(isPasswordValid);
+
+    if (!isUsernameValid) {
+      setUsernameError(
+        "Username must be at least 4 characters long and contain only letters, numbers, and certain special characters.",
+      );
+    } else {
+      setUsernameError("");
+    }
+
+    if (!isPasswordValid) {
+      setPasswordError(
+        "Password must be at least 8 characters long and contain at least one number, one special character, and one uppercase letter.",
+      );
+    } else {
+      setPasswordError("");
+    }
+  }, [formData]);
+
+  const handleFocus = (e) => {
+    setFocusedField(e.target.name);
+  };
+
+  const handleBlur = () => {
+    setFocusedField(null);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("submitted");
+
+    // TODO: Add API call to login and store JWT in context. If successful, redirect to dashboard. If error, display error.
+  };
+
   return (
     // TODO: Add Login Business Logic
     <div className="flex flex-grow items-center justify-center">
-      <div className="dashboard-component p-16 pb-2">
-        <div className="flex h-full flex-col items-center justify-between">
+      <div className="dashboard-component w-4/12 p-16 pb-2">
+        <div className="flex h-full w-full flex-col items-center justify-between">
           <h1 className="mb-8 flex w-full items-center justify-center">
             <FontAwesomeIcon
               className="pr-2 text-3xl text-blue-500"
@@ -22,12 +87,50 @@ export default function Login() {
               <label className="auth-input-label">Username</label>
               <input
                 type="text"
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
                 autoComplete="true"
-                className="auth-input-box"
+                required
+                className={
+                  usernameValid ? "auth-input-box" : "auth-input-box-disabled"
+                }
               />
+              {focusedField === "username" && usernameError && (
+                <div className="auth-input-error">{usernameError}</div>
+              )}
               <label className="auth-input-label">Password</label>
-              <input type="password" className="auth-input-box" />
-              <button className="auth-input-submit">Log in</button>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                required
+                className={
+                  passwordValid ? "auth-input-box" : "auth-input-box-disabled"
+                }
+              />
+              {focusedField === "password" && passwordError && (
+                <div className="auth-input-error">{passwordError}</div>
+              )}
+              <button
+                type="submit"
+                disabled={!usernameValid || !passwordValid}
+                onSubmit={handleSubmit}
+                className={
+                  usernameValid && passwordValid
+                    ? "auth-submit"
+                    : "auth-submit-disabled"
+                }
+              >
+                Log in
+              </button>
             </form>
           </div>
         </div>
