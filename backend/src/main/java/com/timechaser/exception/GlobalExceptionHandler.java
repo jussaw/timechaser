db8b5fac73f3c9e9ab32bdb5b4f64b9839f930e2
Hugh Williams Jr <hugh.r.williams@rtx.com>
2024-06-24 10:20:08 -0600

    Updated global exception handler

--- backend/src/main/java/com/timechaser/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/timechaser/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.timechaser.exception;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -16,6 +21,19 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<?> handleException(Exception e, WebRequest request){
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> validationError(MethodArgumentNotValidException ex) {
+	    StringBuilder builder = new StringBuilder();
+	    
+		BindingResult result = ex.getBindingResult();
+		
+	    List<FieldError> errors = result.getFieldErrors();
+	    for (FieldError error : errors ) {
+	       builder.append(error.getField() + " : " + error.getDefaultMessage());
+	    } 
+		return new ResponseEntity<>(builder.toString(), HttpStatus.BAD_REQUEST);
 	}
 
 }
