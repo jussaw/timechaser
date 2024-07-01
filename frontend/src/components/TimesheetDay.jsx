9fa9b9c7e1fbed685add2f94838112b2f69c3dbb
Justin Sawyer <justin.sawyer@rtx.com>
2024-07-01 08:46:02 -0600

    Add loading icon and new submission functionality

--- frontend/src/components/TimesheetDay.jsx
+++ frontend/src/components/TimesheetDay.jsx
@@ -1,15 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMinus,
   faClock,
   faClipboard,
   faTrash,
+  faPlus,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function TimesheetDay(props) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [showInputs, setShowInputs] = useState(false);
+  const [formData, setFormData] = useState({
+    project: null,
+    hours: null,
+  });
+  const hoverRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+
+    const node = hoverRef.current;
+    if (node) {
+      node.addEventListener("mouseenter", handleMouseEnter);
+      node.addEventListener("mouseleave", handleMouseLeave);
+
+      // Clean up event listeners on component unmount
+      return () => {
+        node.removeEventListener("mouseenter", handleMouseEnter);
+        node.removeEventListener("mouseleave", handleMouseLeave);
+      };
+    }
+  }, [hoverRef.current]);
+
+  const handlePlusClick = () => {
+    setShowInputs(true);
+  };
+
+  const handleMinusClick = () => {
+    setShowInputs(false);
+    props.setTimesheetInput((prevState) => ({
+      ...prevState,
+      [props.day]: {
+        ...prevState[props.day],
+        hours: "",
+        project: "",
+      },
+    }));
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    props.setTimesheetInput((prevState) => ({
+      ...prevState,
+      [props.day]: {
+        ...prevState[props.day],
+        [name]: value,
+      },
+    }));
+  };
+
   return (
-    <div className="flex min-h-full flex-col items-center justify-between rounded-xl shadow-lg">
+    <div
+      ref={hoverRef}
+      className="flex h-full w-full flex-col items-center justify-between rounded-xl shadow-lg"
+    >
       <div className="flex w-full flex-col items-center">
         <div className="flex w-full flex-col justify-center rounded-t-xl bg-blue-500 py-2 text-gray-100">
           <h2 className="text-center text-lg">{props.day}</h2>
@@ -31,29 +88,46 @@ export default function TimesheetDay(props) {
                   <FontAwesomeIcon icon={faMinus} />
                 </div>
               </div>
-              {index < props.projects.length - 1 && (
-                <div className="flex justify-center">
-                  <hr className="w-10/12 border-t-2 bg-gradient-to-r from-gray-100 via-gray-900 to-transparent px-2" />
-                </div>
-              )}
+              <div className="flex justify-center">
+                <hr className="w-11/12 border-t-2 bg-gradient-to-r from-gray-100 via-gray-900 to-transparent px-2" />
+              </div>
             </li>
           ))}
+          <div className="flex w-full justify-center">
+            {isHovered && !showInputs && (
+              <button onClick={handlePlusClick} className="mt-2">
+                <FontAwesomeIcon className="text-gray-500" icon={faPlus} />
+              </button>
+            )}
+            {showInputs && (
+              <form className="mt-3 w-full px-2">
+                <div className="mb-2 flex">
+                  <input
+                    className="text-md mr-1 w-3/12 rounded-lg border border-blue-500 bg-gray-100 px-1"
+                    id="hours"
+                    name="hours"
+                    value={props.timesheetInput[props.day].hours}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="text-md mr-1 w-8/12 rounded-lg border border-blue-500 bg-gray-100 px-1"
+                    id="project"
+                    name="project"
+                    value={props.timesheetInput[props.day].project}
+                    onChange={handleChange}
+                  />
+                  <button
+                    onClick={handleMinusClick}
+                    className="ml-2 w-1/12 items-center text-end text-red-500"
+                  >
+                    <FontAwesomeIcon icon={faXmark} />
+                  </button>
+                </div>
+              </form>
+            )}
+          </div>
         </ul>
       </div>
-      <form className="mb-2 w-full px-2">
-        <div className="">
-          <div className="mb-2 flex">
-            <input
-              className="mr-1 w-9/12 rounded-lg border border-blue-500 px-2 py-1"
-              placeholder="Hours"
-            />
-            <input
-              className="ml-1 w-full rounded-lg border border-blue-500 px-2 py-1"
-              placeholder="Project"
-            />
-          </div>
-        </div>
-      </form>
     </div>
   );
 }
