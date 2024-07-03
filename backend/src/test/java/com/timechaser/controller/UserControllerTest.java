b36ddff2b8f2905d4641b2a74b9c2e4e51dbba8c
Camdyn Ashcraft <Camdyn.Ashcraft@rtx.com>
2024-07-03 09:59:05 -0600

    Reverting import statement

--- backend/src/test/java/com/timechaser/controller/UserControllerTest.java
+++ backend/src/test/java/com/timechaser/controller/UserControllerTest.java
@@ -14,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.timechaser.dto.CreateUserRequest;
