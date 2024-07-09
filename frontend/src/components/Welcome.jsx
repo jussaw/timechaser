6a035b8641ac12add98938528c839cc81c41f02e
Justin Sawyer <justin.sawyer@rtx.com>
2024-07-09 08:46:58 -0600

    Update text and get dates from current week

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+  getSundayOfCurrentWeek,
+  getDateAfterXDays,
+  getWelcomeFormattedDate,
+} from "../utils/DateHelper";
 
 export default function Welcome() {
   // TODO: Replace firstName with what is in AuthContext
   const [firstName, setFirstName] = useState("Bob");
   // TODO: Replace totalHours from API call
-  const [totalHours, setTotalHours] = useState(16);
+  const [workedHours, setWorkedHours] = useState(16);
   // TODO: Repace weekEndDate and weekStartDate with API call
-  const [weekStartDate, setWeekStartDate] = useState("Sun, July 7, 2024");
-  const [weekEndDate, setWeekEndDate] = useState("Sun, July 14, 2024");
+  const [weekStartDate, setWeekStartDate] = useState();
+  const [weekEndDate, setWeekEndDate] = useState();
+
+  useEffect(() => {
+    const sunday = getSundayOfCurrentWeek();
+    setWeekStartDate(getWelcomeFormattedDate(sunday));
+    setWeekEndDate(getWelcomeFormattedDate(getDateAfterXDays(sunday, 7)));
+  }, []);
 
   return (
     <div className="dashboard-component flex h-3/6 w-full flex-grow">
@@ -21,8 +32,8 @@ export default function Welcome() {
         </div>
       </div>
       <div className="bg-custom-purple m-6 ml-0 flex w-4/12 flex-col items-start justify-center rounded-3xl px-8">
-        <div className="text-lg font-semibold">Total worked hours:</div>
-        <div className="text-2xl font-bold">{totalHours} hours</div>
+        <div className="text-lg font-semibold">Worked hours:</div>
+        <div className="text-2xl font-bold">{workedHours} hours</div>
       </div>
     </div>
   );
