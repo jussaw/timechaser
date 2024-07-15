8906716271ecac3a081c9b597a911ca91b1a0dff
Jonah Leung <Jonah.Leung@rtx.com>
2024-07-15 14:45:04 -0600

    resize background image

--- frontend/src/components/PtoRemaining.jsx
+++ frontend/src/components/PtoRemaining.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import crescent from "../assets/crescent.png";
+import crescent from "../assets/crescent.png";
 
 export default function PtoRemaining() {
   const [hours, setHours] = useState();
@@ -8,7 +8,12 @@ export default function PtoRemaining() {
   return (
     <div
       className="dashboard-component h-6/12 relative w-full flex-grow items-center justify-center bg-cover bg-center"
-      // style={{ backgroundImage: `url(${crescent})` }}
+      style={{
+        backgroundImage: `url(${crescent})`,
+        backgroundSize: "50%",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "left",
+      }}
     >
       <div className="m-4 ml-6 flex w-9/12 flex-col rounded-3xl">
         <div className="text-lg font-semibold">Pto remaining:</div>
--- frontend/src/pages/Sandbox.jsx
+++ frontend/src/pages/Sandbox.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import PlaceholderComponent from "../components/PlaceholderComponent";
 import "../styles/UniversalComponent.css";
+import HourAllocationChart from "../components/HourAllocationChart";
+import Welcome from "../components/Welcome";
+import PtoRemaining from "../components/PtoRemaining";
+import star from "../assets/star.png";
 
 export default function Sandbox() {
   return (
-    <div className="flex flex-grow">
-      {/* Comment out this div */}
-      <div className="full-page-component flex-grow items-center justify-center">
-        <div className="flex h-full items-center justify-center">Sandbox</div>
+    <div className="flex w-6/12 flex-col space-y-4">
+      <div className="flex h-full w-full flex-col space-y-4">
+        <div className="flex flex-row space-x-4">
+          <div
+            className="dashboard-component h-6/12 relative w-full flex-grow items-center justify-center bg-cover bg-center"
+            style={{ backgroundImage: `url(${star})` }}
+          >
+            <div className="text-lg font-semibold">Pto remaining:</div>
+            <div className="ml-auto text-3xl font-semibold">10 hours</div>
+          </div>
+          <PlaceholderComponent />
+        </div>
       </div>
     </div>
   );
