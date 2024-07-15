fd0c30fc4b4c344f9137b056e5931e018bb50122
Hugh Williams Jr <hugh.r.williams@rtx.com>
2024-07-15 13:37:14 -0600

    SCD-551 - Added Project Entity

---
+++ backend/src/main/java/com/timechaser/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.timechaser.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.timechaser.entity.Project;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Long, Project>{
+}
