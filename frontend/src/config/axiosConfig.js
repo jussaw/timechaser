19cefb488d05d1b35e5cea20b3bba22a82a61d61
Justin Sawyer <justin.sawyer@rtx.com>
2024-07-16 10:21:38 -0600

    Add axios config

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,43 +1,26 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_SPRING_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// TODO: Add JWT to request
-// Request interceptor for adding JWT
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     // Retrieve token from localStorage, cookies, or wherever you store it
-//     const token = localStorage.getItem('accessToken');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// TODO: implement
-// Response interceptor for refreshing token on 401 responses
-// axiosInstance.interceptors.response.use(
-//   (response) => {
-//     // Handle and refresh token if expired, for example
-//     return response;
-//   },
-//   (error) => {
-//     // Handle 401 Unauthorized responses
-//     if (error.response.status === 401) {
-//       // Logic to handle token refresh or logout user
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.request.use(
+  (config) => {
+    if (!config.headers["Skip-Auth"]) {
+      const token = JSON.parse(localStorage.getItem("authData"))["token"];
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
