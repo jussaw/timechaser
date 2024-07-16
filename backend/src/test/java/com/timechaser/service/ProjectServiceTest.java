432a36b72c5cdc31d43259cf4d72b90fc5ef1c56
Camdyn Ashcraft <Camdyn.Ashcraft@rtx.com>
2024-07-16 13:04:18 -0600

    Created ProjectService unit test

---
+++ backend/src/test/java/com/timechaser/service/ProjectServiceTest.java
@@ -0,0 +1,59 @@
+package com.timechaser.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.timechaser.dto.CreateProjectResponse;
+import com.timechaser.entity.Project;
+import com.timechaser.exception.ProjectCreationException;
+import com.timechaser.repository.ProjectRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ProjectServiceTest {
+
+    @Mock
+    private ProjectRepository projectRepository;
+
+    @InjectMocks
+    private ProjectService projectService;
+
+    private Project project;
+    private String projectName;
+
+    @BeforeEach
+    void setUp() {
+        projectName = "Test Project";
+
+        project = new Project();
+        project.setName(projectName);
+    }
+
+    @Test
+    void ProjectService_Create_Success() {
+        when(projectRepository.save(any(Project.class))).thenReturn(project);
+
+        CreateProjectResponse response = projectService.create(projectName);
+
+        assertNotNull(response);
+        assertEquals(project.getId(), response.getId());
+        assertEquals(project.getName(), response.getName());
+    }
+
+    @Test
+    void ProjectService_Create_Failure() {
+        when(projectRepository.save(any(Project.class))).thenThrow(new IllegalArgumentException());
+
+        assertThatThrownBy(() -> projectService.create(projectName))
+            .isInstanceOf(ProjectCreationException.class);
+    }
+    
+}
