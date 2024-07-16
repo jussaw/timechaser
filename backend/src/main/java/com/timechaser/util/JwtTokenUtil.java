9a54ad7a1ab3191c05daa1b8d5a1808f5b9cf71c
Hugh Williams Jr <hugh.r.williams@rtx.com>
2024-07-16 09:13:31 -0600

    Refactor validate JWT method

--- backend/src/main/java/com/timechaser/util/JwtTokenUtil.java
+++ backend/src/main/java/com/timechaser/util/JwtTokenUtil.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 
@@ -31,10 +32,9 @@ public class JwtTokenUtil {
                 .sign(getAlgorithm());
     }
 
-    public boolean validateToken(String token) {
+    public void validateToken(String token) throws JWTVerificationException {
         JWTVerifier verifier = JWT.require(getAlgorithm()).build();
         verifier.verify(token);
-        return true;
     }
 
     public String getUsernameFromToken(String token) {
