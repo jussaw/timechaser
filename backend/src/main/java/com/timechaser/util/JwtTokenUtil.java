fea1f3a74110315c0635f5679c585a44e4a17eb6
Hugh Williams Jr <hugh.r.williams@rtx.com>
2024-07-16 09:06:30 -0600

    Return 403s when encountering JWT and Authorization errors

--- backend/src/main/java/com/timechaser/util/JwtTokenUtil.java
+++ backend/src/main/java/com/timechaser/util/JwtTokenUtil.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 
@@ -33,13 +32,9 @@ public class JwtTokenUtil {
     }
 
     public boolean validateToken(String token) {
-        try {
-            JWTVerifier verifier = JWT.require(getAlgorithm()).build();
-            verifier.verify(token);
-            return true;
-        } catch (JWTVerificationException exception) {
-            return false;
-        }
+        JWTVerifier verifier = JWT.require(getAlgorithm()).build();
+        verifier.verify(token);
+        return true;
     }
 
     public String getUsernameFromToken(String token) {
