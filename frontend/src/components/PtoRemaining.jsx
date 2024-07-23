2b61edcd7885a20eddeff79cef9a4f2d060b8ecb
Justin Sawyer <justin.sawyer@rtx.com>
2024-07-23 09:02:24 -0600

    Update styling on PTO Remaining component

--- frontend/src/components/PtoRemaining.jsx
+++ frontend/src/components/PtoRemaining.jsx
@@ -1,30 +1,27 @@
 import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-
-import crescent from "../assets/crescent.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 export default function PtoRemaining() {
   const { authData, setAuthData } = useContext(AuthContext);
   const [hours, setHours] = useState(10);
+
   //TODO: retrieve pto from APi call
   useEffect(() => {
     if (authData) {
     }
   }, [authData]);
+
   return (
-    <div
-      className="dashboard-component h-6/12 relative w-full flex-grow items-center justify-center bg-custom-white bg-cover bg-center"
-      style={{
-        backgroundImage: `url(${crescent})`,
-        backgroundSize: "50%",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "left",
-        backgroundColor: "#F3F4F6",
-      }}
-    >
-      <div className="m-4 ml-6 flex w-9/12 flex-col rounded-3xl">
-        <div className="text-lg font-semibold">Pto remaining:</div>
-        <div className="ml-auto text-3xl font-semibold">{hours} hours</div>
+    <div className="dashboard-component flex w-full items-center justify-center bg-custom-white">
+      <FontAwesomeIcon
+        className="-rotate-12 text-9xl text-blue-300"
+        icon={faMoon}
+      />
+      <div className="flex flex-col items-end space-y-2 rounded-3xl text-3xl">
+        <div className="font-semibold">PTO remaining:</div>
+        <div className="font-bold">{hours} hours</div>
       </div>
     </div>
   );
