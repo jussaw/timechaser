9f3b6f9622b8f9f0687ff0bd0128361914b1c7ce
danwegh <george.d.wegh@rtx.com>
2024-07-24 11:46:17 -0600

    whoops - didn't want this change

--- backend/src/main/java/com/timechaser/repository/ProjectRepository.java
+++ backend/src/main/java/com/timechaser/repository/ProjectRepository.java
@@ -13,5 +13,4 @@ public interface ProjectRepository extends JpaRepository<Project, Long>{
 
     public Optional<Project> findByName(String name);
     
-    public Optional<Project> findById(long id);
 }
