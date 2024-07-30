200b04f340bbb3d3451366a2f2cfa2cfaf49b149
Justin Sawyer <justin.sawyer@rtx.com>
2024-07-30 09:36:00 -0600

    Update ManagerMessage size and message

--- frontend/src/components/ManagerMessage.jsx
+++ frontend/src/components/ManagerMessage.jsx
@@ -5,16 +5,16 @@ export default function ManagerMessage() {
 
   //TODO: Set message from API
   useEffect(() => {
-    setMessage("Taco Tuesday at Tacos Selene 🌮🌮🌮  ");
+    setMessage("Taco Tuesday at Tacos Selene 🌮🌮🌮");
   }, []);
 
   return (
-    <div className="dashboard-component flex h-1/2 w-full flex-col bg-custom-white">
+    <div className="dashboard-component flex h-full w-full flex-col bg-custom-white">
       <h1 className="w-full p-4 pb-0 text-center text-2xl font-semibold">
         Message from Manager
       </h1>
       <div className="m-6 flex h-full rounded-3xl bg-gray-200 p-4 px-6 text-start text-lg font-medium shadow-inner">
-        {message ? message : "No message from manager"}
+        {message || "No message from manager"}
       </div>
     </div>
   );
