1bea988be314328a474ce5e50e3b11adcc47da9b
Jonah Leung <Jonah.Leung@rtx.com>
2024-08-06 09:15:31 -0600

    Reformat error messages

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -344,83 +344,94 @@ export default function Profile() {
           Password must be at least 8 characters long and contain at least one
           number, one special character, and one uppercase letter.
         </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col">
+            <form onSubmit={handlePasswordSubmit} className="w-full space-y-8">
+              <div className="entry">
+                <label htmlFor="newPassword" className="password-label">
+                  <span>New Password: </span>
+                </label>
+                <input
+                  className="data input w-32"
+                  type="password"
+                  id="newPassword"
+                  name="newPassword"
+                  value={passwordFormValues.newPassword}
+                  onChange={handlePasswordChange}
+                  autoComplete="off"
+                  required
+                ></input>
+              </div>
 
-        <form onSubmit={handlePasswordSubmit} className="w-full space-y-9">
-          <div className="entry">
-            <label htmlFor="newPassword" className="password-label">
-              <span>New Password: </span>
-            </label>
-            <input
-              className="data input w-32"
-              type="password"
-              id="newPassword"
-              name="newPassword"
-              value={passwordFormValues.newPassword}
-              onChange={handlePasswordChange}
-              autoComplete="off"
-              required
-            ></input>
+              <div className="entry">
+                <label htmlFor="confirmPassword" className="password-label">
+                  <span>Confirm Password: </span>
+                </label>
+                <input
+                  className="data input w-32"
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  value={passwordFormValues.confirmPassword}
+                  onChange={handlePasswordChange}
+                  autoComplete="off"
+                  required
+                ></input>
+              </div>
+              <button
+                type="submit"
+                className={`rounded-full p-2 px-4 text-custom-white ${
+                  isValidPasswords
+                    ? "bg-custom-blue hover:bg-custom-blue-dark"
+                    : "bg-custom-disable"
+                }`}
+                disabled={!isValidPasswords}
+              >
+                Reset
+              </button>
+            </form>
           </div>
-
-          <div className="entry">
-            <label htmlFor="confirmPassword" className="password-label">
-              <span>Confirm Password: </span>
-            </label>
-            <input
-              className="data input w-32"
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              value={passwordFormValues.confirmPassword}
-              onChange={handlePasswordChange}
-              autoComplete="off"
-              required
-            ></input>
-          </div>
-          <button
-            type="submit"
-            className={`rounded-full p-2 px-4 text-custom-white ${
-              isValidPasswords
-                ? "bg-custom-blue hover:bg-custom-blue-dark"
-                : "bg-custom-disable"
-            }`}
-            disabled={!isValidPasswords}
-          >
-            Reset
-          </button>
-          {isPasswordSubmitSuccess && (
-            <div className="w-fit rounded-md bg-custom-green-light px-3 py-2 text-custom-green-dark">
-              🎉 Successfully changed password
-            </div>
-          )}
-          {passwordSubmissionError && (
-            <div className="w-fit rounded-md bg-custom-red-light px-3 py-2">
-              <FontAwesomeIcon className="mr-2" icon={faTriangleExclamation} />
-              {passwordSubmissionError}
-            </div>
-          )}
-          <div className="flex flex-col space-y-1 text-custom-red">
-            {passwordFormValues.newPassword.length > 0 &&
-              !isNewPasswordValid && (
-                <span className="w-fit rounded-md bg-custom-red-light px-3 py-2 text-custom-red">
+          <div className="mx-14 flex flex-col">
+            {isPasswordSubmitSuccess && (
+              <div className="mb-7 h-fit w-fit rounded-md bg-custom-green-light px-3 py-1 text-custom-green-dark">
+                🎉 Successfully changed password
+              </div>
+            )}
+            {passwordSubmissionError && (
+              <div className="h-fit w-fit rounded-md bg-custom-red-light px-3 py-1">
+                <FontAwesomeIcon
+                  className="mr-2"
+                  icon={faTriangleExclamation}
+                />
+                {passwordSubmissionError}
+              </div>
+            )}
+            <div className="flex flex-col space-y-1 text-custom-red">
+              {passwordFormValues.newPassword.length > 0 &&
+                !isNewPasswordValid && (
+                  <span className="mb-6 w-fit rounded-md bg-custom-red-light px-3 py-1 text-custom-red">
+                    <FontAwesomeIcon
+                      className="mr-2"
+                      icon={faCircleExclamation}
+                    />
+                    Invalid password
+                  </span>
+                )}
+              {!(
+                passwordFormValues.newPassword ===
+                passwordFormValues.confirmPassword
+              ) && (
+                <span className="w-fit rounded-md bg-custom-red-light px-3 py-1 text-custom-red">
                   <FontAwesomeIcon
                     className="mr-2"
                     icon={faCircleExclamation}
                   />
-                  Invalid password
+                  Passwords do not match
                 </span>
               )}
-            {!(
-              passwordFormValues.newPassword ===
-              passwordFormValues.confirmPassword
-            ) && (
-              <span className="w-fit rounded-md bg-custom-red-light px-3 py-2 text-custom-red">
-                <FontAwesomeIcon className="mr-2" icon={faCircleExclamation} />
-                Passwords do not match
-              </span>
-            )}
+            </div>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
