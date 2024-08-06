0e3420c05a7014e0f0c2b1403357be38f8463434
Jonah Leung <Jonah.Leung@rtx.com>
2024-08-06 13:58:34 -0600

    Delete api called

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -6,8 +6,11 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import axiosInstance from "../config/axiosConfig";
 import "../styles/Admin.css";
+import Select from "react-select";
 
 export default function Admin() {
+  const [allUsers, setAllUsers] = useState(null);
+
   const [formData, setformData] = useState({
     role: "",
     firstName: "",
@@ -29,6 +32,70 @@ export default function Admin() {
   const [submissionUsername, setSubmissionUsername] = useState(null);
   const [isSubmissionSuccess, setIsSubmissionSuccess] = useState(false);
   const [focusedField, setFocusedField] = useState("");
+
+  //delete form fields
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleDeleteChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  const handleDeleteSubmit = (event) => {
+    event.preventDefault();
+    console.log("Selected option:", selectedOption);
+    console.log("Selected option:", selectedOption.value);
+    axiosInstance
+      .delete(`user/${selectedOption.value}`)
+      .then(() => {})
+      .catch((error) => {
+        mapDeleteErrorCode(error);
+        console.error("Error user creation", error.message);
+      });
+    // Perform your submit action here
+  };
+
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      width: "14rem", // equivalent to w-56
+      borderRadius: "9999px", // equivalent to rounded-full
+      borderColor: "#3B82F6", // equivalent to border-custom-blue
+      backgroundColor: "#F3F4F6", // equivalent to bg-custom-white
+      padding: "0 0.25rem",
+      fontSize: "1.125rem", // equivalent to text-lg
+      boxShadow: state.isFocused ? "0 0 0 2px rgba(59, 130, 246, 0.5)" : "none", // focus:ring-2 focus:ring-custom-blue
+      outline: "none", // focus:outline-none
+      height: "2.5rem", // Adjust height to match other form fields
+      display: "flex", // Ensure proper vertical alignment
+      alignItems: "center", // Center align the content vertically
+    }),
+    menu: (provided) => ({
+      ...provided,
+      borderRadius: "0.375rem", // default rounded corners for menu
+      marginTop: "0.2rem", // default margin for menu
+    }),
+    menuList: (provided) => ({
+      ...provided,
+      padding: 0, // default padding for menu list
+    }),
+    option: (provided) => ({
+      ...provided,
+      padding: "0.5rem 1rem", // default padding for options
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: "#000", // default color for single value
+      height: "100%", // Make sure it takes full height of the control
+      display: "flex",
+      alignItems: "center", // Center align the selected value vertically
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: "#6B7280", // Tailwind's gray-500 for placeholder color (adjust as needed)
+      display: "flex",
+      alignItems: "center",
+    }),
+  };
 
   const formErrors = {
     firstName: "First name must not be empty",
@@ -67,6 +134,24 @@ export default function Admin() {
     setIsFormValid(valid);
   }, [isFormDataValid]);
 
+  useEffect(() => {
+    axiosInstance
+      .get("/user/all")
+      .then((response) => {
+        const formattedOptions = response.data.map((user) => ({
+          value: user.id, // Use unique identifier as the value
+          label: `${user.firstName} ${user.lastName}`,
+        }));
+        console.log("hello");
+        console.log(formattedOptions);
+        setAllUsers(formattedOptions);
+      })
+      .catch((error) => {
+        mapGetAllErrorCode(error);
+        console.error("Error user creation", error.message);
+      });
+  }, []);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setformData({
@@ -83,7 +168,7 @@ export default function Admin() {
     setFocusedField("");
   };
 
-  const mapErrorCode = (error) => {
+  const mapCreateErrorCode = (error) => {
     if (error.response) {
       switch (error.response.status) {
         case 400:
@@ -91,6 +176,40 @@ export default function Admin() {
           break;
         case 500:
           setSubmissionError("This username is already in use");
+          break;
+        default:
+          setSubmissionError("Internal Server Error");
+          break;
+      }
+    } else {
+      setSubmissionError("Server Offline");
+    }
+  };
+
+  //TODO: confirm with backend what response codes for findAll endpoint
+  const mapGetAllErrorCode = (error) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 404:
+          setSubmissionError("No users found");
+          break;
+        default:
+          setSubmissionError("Internal Server Error");
+          break;
+      }
+    } else {
+      setSubmissionError("Server Offline");
+    }
+  };
+
+  const mapDeleteErrorCode = (error) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 400:
+          setSubmissionError("Invalid option selected");
+          break;
+        case 404:
+          setSubmissionError("User does not exist");
           break;
         default:
           setSubmissionError("Internal Server Error");
@@ -138,7 +257,7 @@ export default function Admin() {
           });
       })
       .catch((error) => {
-        mapErrorCode(error);
+        mapCreateErrorCode(error);
         console.error("Error user creation", error.message);
       });
   };
@@ -315,52 +434,42 @@ export default function Admin() {
       <h1 className="text-2xl font-semibold">Delete User</h1>
       <form
         className="flex flex-grow flex-col justify-start space-y-8"
-        onSubmit={handleSubmit}
+        onSubmit={handleDeleteSubmit}
       >
         <div className="flex w-full flex-col space-y-8">
           <div className="admin-label-input-error">
             <div className="admin-label-input">
-              <label className="admin-label" htmlFor="confirmPassword">
+              <label className="admin-label" htmlFor="deleteUser">
                 User ID:
               </label>
-              <input
-                className={"admin-input"}
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-                onFocus={handleFocus}
-                onBlur={handleBlur}
+              <Select
+                id="deleteUser"
+                options={allUsers}
+                styles={customStyles}
+                name="deleteUser"
+                value={selectedOption}
+                onChange={handleDeleteChange}
               />
             </div>
           </div>
         </div>
         <button
           className={`w-fit rounded-full p-2 px-8 text-custom-white ${
-            isFormValid
+            selectedOption
               ? "bg-custom-blue hover:bg-custom-blue-dark"
               : "bg-custom-disable"
           }`}
           type="submit"
-          disabled={!isFormValid}
+          disabled={!selectedOption}
         >
-          Create
+          Delete
         </button>
-        {focusedField && !isFormDataValid[focusedField] && (
-          <span className="admin-error">
-            <FontAwesomeIcon className="mr-2" icon={faCircleExclamation} />
-            {formErrors[focusedField]}
-          </span>
-        )}
+
         {submissionError && (
           <span className="admin-error">
             <FontAwesomeIcon className="mr-2" icon={faTriangleExclamation} />
             {submissionError}
           </span>
-        )}
-        {isSubmissionSuccess && (
-          <div className="w-fit rounded-md bg-custom-green-light px-3 py-2 text-custom-green-dark">
-            🎉 Successfully created user: {submissionUsername}
-          </div>
         )}
       </form>
     </div>
